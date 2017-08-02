2544e822422ff140f7e385b87cac1d326c7551d4
arielwulkan <ariel.wulkan@gmail.com>
2017-08-02 17:47:07 -0300

    simple form

--- Gemfile
+++ Gemfile
@@ -54,8 +54,10 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
-#                               Adicionadas ########################
+
 
 gem 'haml'
 
 gem 'paperclip', '~> 4.2'
+
+gem 'simple_form'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
-class Post < ApplicationRecord
+class Post < ApplicationRecord::Base
 
 
-  has_attached_file :image
+  validates :image, presence: true
 
+  has_attached_file :image, styles: { :medium => "640x" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
